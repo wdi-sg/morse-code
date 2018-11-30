2eafaa84e6b7bfeb510f52916c62179727545dfc
hongchiong <hongchiong@gmail.com>
2018-11-30 10:04:14 +0800

    formatting for clearer morse result. added loop to continue based on user input

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-var input = prompt("Please input a sentence or morse code");
+var loop = true;
+while (loop) {
+    var input = prompt("Please input a sentence or morse code");
 
-if (input[0] == "." || input[0] == "-") {
-    var morseArr = input.split(" ");
-    var result = [];
-    for (var i in morseArr) {
-        result.push(Object.keys(alphabet).find(key => alphabet[key] === morseArr[i]))
-    }
-    result = result.toString();
-    alert(result);
+    if (input[0] == "." || input[0] == "-") {
+        var morseArr = input.split(" ");
+        var result = [];
+        for (var i in morseArr) {
+            result.push(Object.keys(alphabet).find(key => alphabet[key] === morseArr[i]))
+        }
+        result = result.toString();
+        alert(result);
 
-} else {
-    var wordArr = input.split("")
-    var result = [];
-    for (var i in wordArr) {
-        result.push(alphabet[Object.keys(alphabet).find(key => key === wordArr[i])])
-        // debugger;
+    } else {
+        var wordArr = input.split("")
+        var result = [];
+        for (var i in wordArr) {
+            result.push(alphabet[Object.keys(alphabet).find(key => key === wordArr[i])])
+        }
+        result = result.join(" | ");
+        alert(result);
     }
-    result = result.toString();
-    alert(result);
+    loop = prompt("Do you want to continue? Enter 'y'");
+    if (loop[0].toLowerCase() == "y") {
+        loop = true;
+    } else {
+        loop = false;
+    }
 }
+
 
 
