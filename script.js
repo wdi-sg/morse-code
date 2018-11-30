e8d01ca0f3fc7a006586d73257df7f694faee694
hongchiong <hongchiong@gmail.com>
2018-11-30 10:10:41 +0800

    refactor core functionality into a function

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 var loop = true;
+var morseCodeEnDecoder = function(input) {
+    if (input[0] == "." || input[0] == "-") {
+    var morseArr = input.split(" ");
+    var result = [];
+    for (var i in morseArr) {
+        result.push(Object.keys(alphabet).find(key => alphabet[key] === morseArr[i]))
+    }
+    result = result.toString();
+    alert(result);
+
+} else {
+    var wordArr = input.split("")
+    var result = [];
+    for (var i in wordArr) {
+        result.push(alphabet[Object.keys(alphabet).find(key => key === wordArr[i])])
+    }
+    result = result.join(" | ");
+    alert(result);
+}
+}
+
 while (loop) {
     var input = prompt("Please input a sentence or morse code");
-
-    if (input[0] == "." || input[0] == "-") {
-        var morseArr = input.split(" ");
-        var result = [];
-        for (var i in morseArr) {
-            result.push(Object.keys(alphabet).find(key => alphabet[key] === morseArr[i]))
-        }
-        result = result.toString();
-        alert(result);
-
-    } else {
-        var wordArr = input.split("")
-        var result = [];
-        for (var i in wordArr) {
-            result.push(alphabet[Object.keys(alphabet).find(key => key === wordArr[i])])
-        }
-        result = result.join(" | ");
-        alert(result);
-    }
+    morseCodeEnDecoder(input);
     loop = prompt("Do you want to continue? Enter 'y'");
     if (loop[0].toLowerCase() == "y") {
         loop = true;
@@ -27,6 +30,3 @@ while (loop) {
         loop = false;
     }
 }
-
-
-
